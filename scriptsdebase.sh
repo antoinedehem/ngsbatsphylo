126f88237d185bb662be31dd83ef88e687052837
Antoine Dehem <antoine.dehem@ens-lyon.fr>
2020-11-16 14:33:41 +0000

    J1.15.32 ajout des scripts de téléchargement

---
+++ scriptsdebase.sh
@@ -0,0 +1,6 @@
+#Scripts de base du TP
+
+#télécharger les fichiers 
+
+cd download
+wget -r --ftp-user=igfl-UE_NGS_2020 --ftp-password=UE_NGS_2020 ftp://sharegate-igfl.ens-lyon.fr/Projet_31_20_UE_NGS_2020/
