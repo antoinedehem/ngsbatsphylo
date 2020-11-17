0eadc18502cdc8ae734c34afe7b55ba0e320450b
Antoine Dehem <antoine.dehem@ens-lyon.fr>
2020-11-17 10:58:21 +0000

    J2.11.56 Boucle faite, test de fastqc

--- scriptfastqc.sh
+++ scriptfastqc.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+
+#créer un dossier pour le fastQC
+
+#cd mydatalocal
+
+#mkdir results
+
+#cd results
+
+#mkdir fastqc
+
+#cd ../..
+
+#vériication de fastq
+fastqc -o /ifb/data/mydatalocal/results /ifb/data/mydatalocal/download/FASTQ/Lib1_31_20_S1_R1_001.fastq.gz
+#boucle d'analyse
+for i in /ifb/data/mydatalocal/download/FASTQ/*.gz
+do
+echo "voici $i"
+fastqc -o /ifb/data/mydatalocal/results $i
+done
+
+#changer le outpout du fastqc
+fastqc -o /ifb/data/mydatalocal/results/fastqc
