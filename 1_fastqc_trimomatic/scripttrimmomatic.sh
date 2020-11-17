18ad0ab81fac9aeb35ac59b227fdb46db431380d
Antoine Dehem <antoine.dehem@ens-lyon.fr>
2020-11-17 15:26:49 +0000

    J2.16.24 Trimmomatic corrections : ce script fonctionne, erreurs de syntaxes corrigées

--- 1_fastqc_trimomatic/scripttrimmomatic.sh
+++ 1_fastqc_trimomatic/scripttrimmomatic.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cleaneddata="/ifb/data/mydatalocal/cleaneddata"
-mkdir -p $cleaneddata
-cd $cleaneddata
+cleandata="/ifb/data/mydatalocal/cleandata"
+mkdir -p $cleandata
+cd $cleandata
 
 home_download="/ifb/data/mydatalocal/download"
 home_fastq=$home_download/"FASTQ"
@@ -10,16 +10,10 @@ fastq=$home_fastq/"*.gz"
 
 for i in 1 2 3 4 5 6 
 do
-#appel du programme
-java -jar /softwares/Trimmomatic-0.39/trimmomatic-0.39.jar\
-#paired end
-PE -phred33\
-#le input
-$home_fastq/Lib1_31_20_S${i}_R1_001 $home_fastq/Lib1_31_20_S${i}_R2_001\
-#les output
-cleaned_paired_S${i}_R1.fq.gz cleaned_unpaired_S${i}_R1.fq.gz\
-cleaned_paired_S${i}_R2.fq.gz cleaned_unpaired_S${i}_R2.fq.gz\
-#les paramètres illuminaclip: Cut adapter and other illumina-specific sequences from the read. 
+echo "Cleaning ${i}"
+#appel du programme#paired end#le input#les output#les paramètres ILLUMINACLIP: Cut adapter and other illumina-specific sequences from the read. 
 #HEADCROP: Cut the specified number of bases from the start of the read
 #MINLEN: Drop the read if it is below a specified length
-ILLUMINACLIP:$home_download/adapt.fasta HEADCROP:5 MINLEN:100
+java -jar /softwares/Trimmomatic-0.39/trimmomatic-0.39.jar PE -phred33 $home_fastq/Lib${i}_31_20_S${i}_R1_001.fastq.gz $home_fastq/Lib${i}_31_20_S${i}_R2_001.fastq.gz Lib${i}_31_20_clean_paired_S${i}_R1.fq.gz Lib${i}_31_20_clean_unpaired_S${i}_R1.fq.gz Lib${i}_31_20_clean_paired_S${i}_R2.fq.gz Lib${i}_31_20_clean_unpaired_S${i}_R2.fq.gz ILLUMINACLIP:$home_download/adapt.fasta:2:30:10 HEADCROP:5 MINLEN:100
+done
+echo "the End!"
