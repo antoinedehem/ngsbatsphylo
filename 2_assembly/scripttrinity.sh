e6d59407ee9507c3594300d0f74229b36505b8c9
Antoine Dehem <antoine.dehem@ens-lyon.fr>
2020-11-18 10:36:22 +0000

    J3.11.34 Trinity Suppression de la boucle for inutile dans ce cas étant donné qu'on donne à manger à Trinity les listes de tous les fichiers paired d'un coup, tentative de régler le soucis du dossier results_trinity inefficace

--- 2_assembly/scripttrinity.sh
+++ 2_assembly/scripttrinity.sh
@@ -17,10 +17,5 @@ home_fastqclean="/ifb/data/mydatalocal/cleandata"
 FQR1=$(ls ${home_fastqclean}/Lib*_31_20_clean_paired*R1.fq.gz |paste -d "," -s)
 FQR2=$(ls ${home_fastqclean}/Lib*_31_20_clean_paired*R2.fq.gz |paste -d "," -s)
 
-
-for i in 1 2 3 4 5 6 
-do
-echo 'Assembly number 1'
-Trinity --seqType fq --max_memory 8G --left $FQR1 --right $FQR2 --SS_lib_type RF --CPU 3 --output $results_trinity 
-done
+Trinity --seqType fq --max_memory 8G --left $FQR1 --right $FQR2 --SS_lib_type RF --CPU 3 --output $results_trinity
 echo 'The end!'
