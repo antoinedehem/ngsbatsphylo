0bf9f58c5a93450eabf2afe7357f4bae9c2ca9b4
Antoine Dehem <antoine.dehem@ens-lyon.fr>
2020-11-18 09:59:31 +0000

    J3.10.56 Trinity modification permettant de construire les listes R1 et R2 que peut traiter Trinity. le test du script est concluant. Cependant le script créé un sous dossier results_trinity en plus dans results_trinity : à corriger

--- 2_assembly/scripttrinity.sh
+++ 2_assembly/scripttrinity.sh
@@ -14,11 +14,13 @@ cd $results_trinity
 #Récupération des données
 
 home_fastqclean="/ifb/data/mydatalocal/cleandata"
-fastqclean=$home_fastqclean/"*.gz"
+FQR1=$(ls ${home_fastqclean}/Lib*_31_20_clean_paired*R1.fq.gz |paste -d "," -s)
+FQR2=$(ls ${home_fastqclean}/Lib*_31_20_clean_paired*R2.fq.gz |paste -d "," -s)
+
 
 for i in 1 2 3 4 5 6 
 do
 echo 'Assembly number 1'
-Trinity --seqType fq --max_memory 8G --left ${home_fastqcleanLib}/${i}_31_20_clean_paired_S${i}_R1.fq.gz --right ${home_fastqcleanLib}/Lib${i}_31_20_clean_paired_S${i}_R1.fq.gz --SS_lib_type RF --CPU 3 --output $results_trinity 
+Trinity --seqType fq --max_memory 8G --left $FQR1 --right $FQR2 --SS_lib_type RF --CPU 3 --output $results_trinity 
 done
 echo 'The end!'
